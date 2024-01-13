5ebe9d87e80d0d7f244d7bcf00d041b819c70ee9
Kenley <1161723373@qq.com>
2024-01-13 23:03:07 +0800

    ListLogGroups方法实现

--- services/lts/log_group.go
+++ services/lts/log_group.go
@@ -1,6 +1,8 @@
 package lts
 
-import ltsModel "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/lts/v2/model"
+import (
+	ltsModel "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/lts/v2/model"
+)
 
 type (
 	LogGroup interface {
@@ -9,5 +11,7 @@ type (
 )
 
 func (c *ltsClient) ListLogGroups() (*ltsModel.ListLogGroupsResponse, error) {
-	return nil, nil
+	request := &ltsModel.ListLogGroupsRequest{}
+	response, err := c.client.ListLogGroups(request)
+	return response, err
 }
